df7d2edf239f93debaef335a3ca74ebabf408a63
houyahui <houyahui@t2cloud.net>
2018-02-09 14:17:46 +0800

    github issueresult print fmt error fix

--- chapter04/issue/main.go
+++ chapter04/issue/main.go
@@ -14,6 +14,6 @@ func main() {
 	}
 	fmt.Printf("%d issues: \n", result.TotalCount)
 	for _, item := range result.Items {
-		fmt.Printf("#%-5d %9.9s %.55\n", item.Number, item.User.Login, item.Title)
+		fmt.Printf("#%-5d %9.9s %.55s \n", item.Number, item.User.Login, item.Title)
 	}
 }
