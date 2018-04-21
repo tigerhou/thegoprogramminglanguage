bc0da396a33d1e5c0fb2386a68db0ffee26532f9
houyahui <houyahui@t2cloud.net>
2018-04-21 10:00:02 +0800

    goroutine 更新

---
+++ chapter08/clock1.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"time"
+)
+
+func main() {
+
+	var port = flag.String("port", "8080", "")
+	flag.Parse()
+
+	fmt.Println("------port:" + *port)
+
+	listener, err := net.Listen("tcp", "localhost:"+*port)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		go handleConn(conn)
+	}
+}
+
+func handleConn(c net.Conn) {
+	defer c.Close()
+	for {
+		_, err := io.WriteString(c, time.Now().Format("15:04:05\n"))
+		if err != nil {
+			return
+		}
+		time.Sleep(1 * time.Second)
+	}
+}
